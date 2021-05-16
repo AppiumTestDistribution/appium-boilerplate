4177a811bc37c442d9f99f04875e6e32cb68b8ed
saikrishna321 <saikrishna321@yahoo.com>
2021-05-16 19:53:36 +0530

    update config

--- config/wdio.shared.conf.js
+++ config/wdio.shared.conf.js
@@ -1,45 +1,43 @@
 exports.config = {
-    // ====================
-    // Runner and framework
-    // Configuration
-    // ====================
     runner: 'local',
     framework: 'jasmine',
     jasmineNodeOpts: {
-        // Updated the timeout to 30 seconds due to possible longer appium calls
-        // When using XPATH
         defaultTimeoutInterval: 90000,
         helpers: [require.resolve('@babel/register')],
     },
     sync: true,
-    logLevel: 'silent',
+    logLevel: 'info',
+    path: '/wd/hub',
     deprecationWarnings: true,
     bail: 0,
     baseUrl: 'http://the-internet.herokuapp.com',
     waitforTimeout: 10000,
-    connectionRetryTimeout: 90000,
-    connectionRetryCount: 3,
+    connectionRetryCount: 0,
     reporters: ['spec'],
 
     // ====================
     // Appium Configuration
     // ====================
-    services: [
-        [
-            'appium',
-            {
-            // For options see
-            // https://github.com/webdriverio/webdriverio/tree/master/packages/wdio-appium-service
-                args: {
-                    // Auto download ChromeDriver
-                    relaxedSecurity: true,
-                    // chromedriverAutodownload: true,
-                    // For more arguments see
-                    // https://github.com/webdriverio/webdriverio/tree/master/packages/wdio-appium-service
-                },
-                command: 'appium',
-            },
-        ],
-    ],
+    // services: [
+    //     [
+    //         "appium",
+    //         {
+    //             // For options see
+    //             // https://github.com/webdriverio/webdriverio/tree/master/packages/wdio-appium-service
+    //             args: {
+    //                 // Auto download ChromeDriver
+    //                 relaxedSecurity: true,
+    //                 logPath: "./",
+    //                 plugins: "element-wait,device-manager",
+    //                 "base-path": "/wd/hub",
+    //                 // chromedriverAutodownload: true,
+    //                 // For more arguments see
+    //                 // https://github.com/webdriverio/webdriverio/tree/master/packages/wdio-appium-service
+    //             },
+    //             command:
+    //                 "/Users/saikrisv/.node/lib/node_modules/appium/build/lib/main.js",
+    //         },
+    //     ],
+    // ],
     port: 4723,
 };
